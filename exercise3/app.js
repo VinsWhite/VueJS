2d713bd25d6f4da12ed80494fc986622fa5987bd
Vincenzo Saccone <vincenzosa26@icloud.com>
2024-08-12 10:58:22 +0200

    events and v-once

--- exercise3/app.js
+++ exercise3/app.js
@@ -2,9 +2,21 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      /* event.preventDefault(); */ /* vue has a better way to do this */
+      alert('submitted!')
+    },
+    setName(event, ex) {
+      this.name = event.target.value + ' ' + ex;
+    },
     addCounter(num) {
       this.counter = this.counter + num;
     },
