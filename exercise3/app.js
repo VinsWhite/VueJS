9dc2c2aafdd40387968174ffb917afefbda899ec
Vincenzo Saccone <vincenzosa26@icloud.com>
2024-08-16 10:57:21 +0200

    reactivity

--- exercise3/app.js
+++ exercise3/app.js
@@ -3,10 +3,52 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
-      confirmedName: ''
+      confirmedName: '',
+      /* fullname: '', */
+      lastName: '',
     };
   },
+  watch: {
+    /* name(value) { // the name should be the same of data we need to use
+      if (value === '') {
+        this.fullname = '';
+      } else {
+         this.fullname = value + ' ' + this.lastName // value = automatic passed by vue; value = name
+      }
+    },
+    lastName(value) {
+      if (value === '') {
+        this.fullname = '';
+      } else {
+         this.fullname = this.name + ' ' + value;
+      }
+    } */
+   counter(value) {
+    if (value > 50) {
+      this.counter = 0;
+    }
+   }
+  },
+  computed: { /* better performance */
+    fullname() { /* when name changes */
+      console.log('Running again...')
+      if (this.name === '' || this.lastName === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Saccone'
+    }
+  },
   methods: {
+    outputFullname() {
+      console.log('Running again...')
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Saccone'
+    },
+    resetInput() {
+      this.name="";
+    },
     confirmInput() {
       this.confirmedName = this.name;
     },
