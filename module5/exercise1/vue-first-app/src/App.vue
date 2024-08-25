abc550658975fb758aaf3ca68d42a90aaa97b4b9
Vincenzo Saccone <vincenzosa26@icloud.com>
2024-08-25 11:59:38 +0200

    prop behavior, changing props, dynamic prop values

--- module5/exercise1/vue-first-app/src/App.vue
+++ module5/exercise1/vue-first-app/src/App.vue
@@ -1,9 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import FriendContactVue from './components/FriendContact.vue';
+
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+          isFavorite: 1,
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+          isFavorite: 0,
+        },
+      ],
+    };
+  },
+  components: {
+    FriendContactVue
+  }
+};
 </script>
 
 <template>
-  <div>
-    <!-- <HelloWorld msg="Vite + Vue" /> -->
-  </div>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <FriendContactVue v-for="friend in friends" :key="friend.id" :friend="friend" />
+    </ul>
+  </section>
 </template>
---
+++ module5/exercise1/vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  props: {
+    friend: { // we passed as an object and it should be required
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      if (this.friendIsFavorite === 1) {
+        this.friendIsFavorite = 0;
+      } else {
+        this.friendIsFavorite = 1;
+      }
+    }
+  }
+};
+</script>
+
+
+<template>
+  <li>
+    <h2>{{ friend.name }} {{ friendIsFavorite === 1 ? '(Favorite)' : '' }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong> {{ friend.phone }}
+      </li>
+      <li>
+        <strong>Email:</strong> {{ friend.email }}
+      </li>
+    </ul>
+  </li>
+</template>
