8e0dfe8e8f1b2a6a18a5395612f331b8ead08741
Vincenzo Saccone <vincenzosa26@icloud.com>
2024-08-30 18:09:04 +0200

    global and local components, scoped styles, slots, named slotes

---
+++ module6/badge/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import TheHeaderVue from './components/TheHeader.vue'
+import BadgeListVue from './components/BadgeList.vue'
+import UserInfoVue from './components/UserInfo.vue'
+
+export default {
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+  components: {
+    TheHeaderVue,
+    BadgeListVue,
+    UserInfoVue
+  }
+};
+</script>
+
+<template>
+  <div>
+    <TheHeaderVue></TheHeaderVue>
+    <BadgeListVue></BadgeListVue>
+    <UserInfoVue
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></UserInfoVue>
+  </div>
+</template>
