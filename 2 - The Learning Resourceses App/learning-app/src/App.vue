3ee1030491cd3c73ee4556fd2a42274748b928a8
Vincenzo Saccone <vincenzosa26@icloud.com>
2024-09-02 12:16:22 +0200

    update

---
+++ 2 - The Learning Resourceses App/learning-app/src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <ul>
+      <LearningResource 
+        v-for="resource in storedResources" 
+        :key="resource.id"
+        :title="resource.title"
+        :description="resource.description"
+        :link="resource.link"
+      >
+      </LearningResource>
+    </ul>
+  </div>
+</template>
+
