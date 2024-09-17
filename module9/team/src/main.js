b04777a57155368a69b73288bed731bf5efb9062
Vincenzo Saccone <vincenzosa26@icloud.com>
2024-09-17 16:55:00 +0200

    navigation

--- module9/team/src/main.js
+++ module9/team/src/main.js
@@ -4,12 +4,14 @@ import { createRouter, createWebHistory } from 'vue-router' // to create a route
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from'./components/users/UsersList.vue'
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({ // we need to which url we want to support
     history: createWebHistory(),
     routes: [
         { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
         { path: '/users', component: UsersList },
+        { path: '/teams/:teamId', component: TeamMembers },
     ],
     linkActiveClass: 'active'
 }); 
